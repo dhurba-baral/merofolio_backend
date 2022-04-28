d024077ab7e7b7d711c7f33c127dba202df644a2
Pallavi Pandey <pallavipandey299@gmail.com>
2022-04-28 16:34:52 +0545

    Notification email

--- src/authentication/alertmail.js
+++ src/authentication/alertmail.js
@@ -3,8 +3,10 @@ const Notification = require('../models/notify');
 const mail = require('./sendemail')
 const axios = require('axios').default;
 
+//check every 30 seconds
 let sendAlert = cron.schedule("*/30 * * * * *", async function () {
     try {
+        //find every notification
         const notifications = await Notification.find()
         notifications.forEach(async function (notify) {
             let price = notify.stockprice
@@ -13,13 +15,14 @@ let sendAlert = cron.schedule("*/30 * * * * *", async function () {
             axios.get(`http://localhost:3000/api/livedata`)
                 .then(response => {
                     response.data.forEach(async function (asset) {
+                        //compare with the stock
                         if (asset.Companyname === stock) {
                             let value = parseInt(asset.Ltp)
                             if (value <= price) {
                                 let text = `The price of ${stock} has reached below your preferred amount of ${price}. The current price is ${asset.Ltp}`
                                 let subject = `${stock} price fell `
                                 console.log(subject)
-                                // await mail(email, subject, text)
+                                await mail(email, subject, text)
                                 await Notification.findByIdAndDelete(notify._id)
                             }
                         }
